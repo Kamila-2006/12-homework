ac74918327db0b9a8439cd5d7535785ddeed114e
Kamila <abdurahmanovakamila.2006@gmail.com>
2024-12-22 20:00:40 +0500

    updated

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 
 app_name = 'products'
@@ -13,5 +12,3 @@ urlpatterns = [
     path('create/', views.create_product, name='create'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
